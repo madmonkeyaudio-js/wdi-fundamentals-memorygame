72b5b42ba4c8907c47f0c01905a1fa27dd147f6a
Jonathan Gross <jonathangross@Jonathans-MBP.hsd1.wa.comcast.net>
2019-06-13 09:52:06 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -6,20 +6,34 @@ var cards = ["queen", "queen", "king", "king"]
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
+function checkForMatch() {
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
+	if (cardsInPlay.length === 2) {
  if (cardsInPlay[0] === cardsInPlay [2]) {
-	alert("You found a match!");
+	console.log("You found a match!");
 } else {
-	alert("Sorry, try again.");
+	console.log("Sorry, try again.");
 }
 }
+
+
+}
+
+function flipCard(cardId) {
+
+	cardsInPlay.push(cards[cardId]);
+
+console.log("User flipped " + cards[cardId]);
+
+checkForMatch();
+
+}
+
+
+flipCard(0);
+flipCard(2);
+
+
+
+
 
